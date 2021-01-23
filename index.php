bff8e0ec8444ce5275e607b17ad0ad201f29df29
Venomo <62396650+WokeWorld@users.noreply.github.com>
2021-01-24 09:49:16 +1100

    Update index.php
    up

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ $results = $page;
 // CHECK ALL LOGIN ERRORS
 if (contains("login_error",$results) | contains("Error",$results))
 {
-  $errorMsg=$results;
+  $errorMsg="Username OR Password Incorrect";
       if (contains("is incorrect",$results))
        {
          $errorMsg="Username Correct, Password Failed";
